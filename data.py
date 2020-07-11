4d1767698623afec227d37a66ab847233c334b63
disanda <skyanda1@qq.com>
2020-07-12 00:41:27 +0800

    x

--- data.py
+++ data.py
@@ -9,18 +9,18 @@ from torchvision import datasets, transforms
 #-------------------one-channel dataset-------------
 
 # data
-transform = tforms.Compose(
-    [tforms.Scale(size=(32, 32), interpolation=Image.BICUBIC),
-     tforms.ToTensor(),
-     tforms.Lambda(lambda x: torch.cat((x, x, x), dim=0)),
-     tforms.Normalize(mean=[0.5] * 3, std=[0.5] * 3)]
+transform1 = transforms.Compose(
+    [transforms.Scale(size=(32, 32), interpolation=Image.BICUBIC),
+     transforms.ToTensor(),
+     transforms.Lambda(lambda x: torch.cat((x, x, x), dim=0)),
+     transforms.Normalize(mean=[0.5] * 3, std=[0.5] * 3)]
 )
 
 def getDataloader(batch_size,use_gpu):
     train_loader = torch.utils.data.DataLoader(
     #dataset=dsets.FashionMNIST('data/FashionMNIST', train=True, download=True, transform=transform),
     #dataset=torchvision.datasets.CIFAR10('./data', train=True, download=True, transform=transform),
-    dataset=torchvision.datasets.MNIST('./data/', train=True, download=True, transform=transform),
+    dataset=torchvision.datasets.MNIST('./data/', train=True, download=True, transform=transform1),
     batch_size=batch_size,
     shuffle=True,
     num_workers=4,
