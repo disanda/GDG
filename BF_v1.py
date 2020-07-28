254a4bd6cf7e3bbf733741598e2e6d14ba3a8960
disanda <skyanda1@qq.com>
2020-07-28 23:14:46 +0800

    jul28

---
+++ BF_v1.py
@@ -0,0 +1,27 @@
+from torch.nn import functional as F
+
+class Encode(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 400)
+        self.fc21 = nn.Linear(400, 20)
+        self.fc22 = nn.Linear(400, 20)
+    def forward(self, x):
+        h1 = F.relu(self.fc1(x))
+        mu, logvar = self.fc21(h1), self.fc22(h1)
+        return mu, logvar
+
+class Decode(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(20, 400)
+        self.fc2 = nn.Linear(400, 784)
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)#0-1随机数
+        return mu + eps*std
+    def forward(self, mu, logvar):
+        z = self.reparameterize(mu, logvar)
+        h3 = F.relu(self.fc1(z))
+        x_f = torch.sigmoid(self.fc2(h3))
+        return x_f
--- vae.py
+++ vae.py
@@ -6,66 +6,25 @@ from torch import nn, optim
 from torch.nn import functional as F
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
+import models.BF_v1 as model
 
+#-------------setting---------------
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N',help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',help='number of epochs to train (default: 10)')
+parser.add_argument('--seed', type=int, default=1, metavar='S',help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',help='how many batches to wait before logging training status')
+args.cuda = True
 args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-
 torch.manual_seed(args.seed)
+device = torch.device("cuda")
 
-device = torch.device("cuda" if args.cuda else "cpu")
+#------------------dataSet----------------
+kwargs = {'num_workers': 4, 'pin_memory': True} if args.cuda else {}
+train_loader = torch.utils.data.DataLoader(datasets.MNIST('../data', train=True, download=True,transform=transforms.ToTensor()),batch_size=args.batch_size, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),batch_size=args.batch_size, shuffle=True, **kwargs)
 
-kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=True, download=True,
-                   transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-
-class VAE(nn.Module):
-    def __init__(self):
-        super(VAE, self).__init__()
-
-        self.fc1 = nn.Linear(784, 400)
-        self.fc21 = nn.Linear(400, 20)
-        self.fc22 = nn.Linear(400, 20)
-        
-        self.fc3 = nn.Linear(20, 400)
-        self.fc4 = nn.Linear(400, 784)
-
-    def encode(self, x):
-        h1 = F.relu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1)
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)#0-1随机数
-        return mu + eps*std
-
-    def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
-
-    def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
-
-model = VAE().to(device)
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
-
+#-------------------loss function--------------------------
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar):
     BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
@@ -76,27 +35,41 @@ def loss_function(recon_x, x, mu, logvar):
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     return BCE + KLD
 
+Encode = model.Encode().to(device)
+Decode = model.Decode().to(device)
+optimizerE = optim.Adam(Encode.parameters(), lr=1e-3)
+optimizerD = optim.Adam(Decode.parameters(), lr=1e-3)
+
 def train(epoch):
-    model.train()
-    train_loss = 0
+    Encode.train()
+    Decode.train()
+    train_loss_KLD = 0
+    train_loss_BCE = 0
     for batch_idx, (data, _) in enumerate(train_loader):
         data = data.to(device)
-        optimizer.zero_grad()
-        recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
-        loss.backward()
-        train_loss += loss.item()
-        optimizer.step()
+        #-----------------training Encode---------------
+        optimizerE.zero_grad()
+        mu, logvar = Encode(data)
+        loss_KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        loss_KLD.backward()
+        train_loss_KLD += loss_KLD.item()
+        optimizerE.step()
+        #-----------------training Decode---------------
+        optimizerD.zero_grad()
+        x_fake = Decode(mu, logvar)
+        loss_BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+        loss_BCE.backward()
+        train_loss_BCE += loss_BCE.item()
+        optimizerD.step()
         if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.item() / len(data)))
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-          epoch, train_loss / len(train_loader.dataset)))
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss_KLD: {:.6f}\tLoss_BCE: {:.6f}'.format(epoch, batch_idx * len(data), len(train_loader.dataset),100. * batch_idx / len(train_loader),loss_KLD.item() / len(data),loss_BCE.item() / len(data)))
+    print('====> Epoch: {} Average loss KLD: {:.4f}--Average loss BCE: {:.4f}'.format(epoch, train_loss_KLD / len(train_loader.dataset) , train_loss_BCE / len(train_loader.dataset)))
+
+if not os.path.exists('results'):
+    os.mkdir('results')
 
 def test(epoch):
-    model.eval()
+    Decode.eval()
     test_loss = 0
     with torch.no_grad():
         for i, (data, _) in enumerate(test_loader):
@@ -117,5 +90,4 @@ if __name__ == "__main__":
         with torch.no_grad():
             sample = torch.randn(64, 20).to(device)
             sample = model.decode(sample).cpu()
-            save_image(sample.view(64, 1, 28, 28),
-                       'results/sample_' + str(epoch) + '.png')
+            save_image(sample.view(64, 1, 28, 28),'results/sample_' + str(epoch) + '.png')
